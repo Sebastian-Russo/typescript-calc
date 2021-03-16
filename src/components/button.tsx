2fb02fe2b4737f59d04ea4e9759cfc7e3e9298f8
Sebastian Russo <russo.sebastian@gmail.com>
2021-03-16 16:59:58 -0400

    edit button component types

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,12 +3,15 @@ import '../styling/button.css';
 
 // create types for props
 type ButtonProps = {
-  handleClick: any;
-  children: any;
+  // HandleClick needs to a be a function type with a parameter that takes a ReactNode (since that's the type of props.children in this case, and you want to pass it into that function
+  handleClick: (children: React.ReactNode) => void; // basically, clicking the reusable component I made, the arg is a ReactNode 
+  // don't need to specify children prop, <ButtonProps> has it by default
 }
 
-const isOperator = (val:any) => {
-  return !isNaN(val) || val === "." || val === "=";
+                // set type for value
+const isOperator = (val: React.ReactNode) => {
+  // isNaN() only takes a number parm, and only checks whether a number is set to specal value of 'NaN'
+  return (typeof val === "number" && !isNaN(val)) || val === "." || val === "=";
 }
 
 // generic type, FC
